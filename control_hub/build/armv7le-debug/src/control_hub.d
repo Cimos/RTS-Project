a4edc194bb3364f213a4f52adf67447e8308dbe3
Simon Maddison <simon.maddison@fiberdyne.com.au>
2018-09-06 13:13:07 +1000

    #1 fixing build .ignore and build folder

--- control_hub/build/armv7le-debug/src/control_hub.d
+++
@@ -1,6 +0,0 @@
-build/armv7le-debug/src/control_hub.o: src/control_hub.cpp \
- src/../../_source/api/i2c_HAL.h src/../../_source/api/../i2c/i2c_HAL.h \
- src/../../_source/api/../i2c/../FT800/FT800.h \
- src/../../_source/api/../i2c/../FT800/FT_DataTypes.h \
- src/../../_source/api/../i2c/../FT800/FT_Gpu.h \
- src/../../_source/api/server_config.h
--- vehicle_intersection_1/build/armv7le-debug/src/vehicle_intersection_1.d
+++
@@ -1,2 +0,0 @@
-build/armv7le-debug/src/vehicle_intersection_1.o: \
- src/vehicle_intersection_1.cpp
