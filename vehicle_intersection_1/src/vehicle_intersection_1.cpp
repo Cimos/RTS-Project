ed7121dfc105dc4a7408b2570975345e8f263ad9
Michael Stekla <michael.stekla@gmail.com>
2018-09-23 16:22:36 +1000

    #1 basic traffic state machine running

--- vehicle_intersection_1/src/vehicle_intersection_1.cpp
+++ vehicle_intersection_1/src/vehicle_intersection_1.cpp
@@ -35,31 +35,70 @@
 /*-----------------------------------------------------------------------------
 * Local Variables and Buffers
 *---------------------------------------------------------------------------*/
-enum TRAFFIC_STATES {
-	INITIALIZE = 0,
-	EWNS_STOP,
-	EWG_NSR,
-	EWY_NSR,
-	EWR_NSR,
-	EWR_NSG,
-	EWR_NSY,
-	INVALID_STATE
+enum TRAFFIC_STATES
+{
+	DEFAULT = 0,
+	NSG,
+	NSY,
+	NSTG,
+	NSTY,
+	EWG,
+	EWY,
+	EWTG,
+	EWTY,
+	RED,
+	ERROR
 };
 
-typedef struct
+enum LIGHT_COLORS
 {
-	enum 	TRAFFIC_STATES current_state;	// Current State of traffic lights
-	char 	sensor;				// Traffic light sensor input
-	int 	counter;			// Runtime counter
-} traffic_data;
+	LED_OFF = 0,
+	LED_GREEN,
+	LED_YELLOW,
+	LED_RED,
+};
 
-typedef struct
+struct TRAFFIC_TIMING
 {
-	int 	pid;				// Server Process ID
-	int 	chid;				// Server Channel ID
-	char 	message;			// Message passed through server
-	int 	running;			// Server running status
-} server_info;
+	int ns_straight;
+	int ns_turn;
+	int ew_straight;
+	int ew_turn;
+};
+
+struct TRAFFIC_LIGHTS
+{
+	LIGHT_COLORS north_s;
+	LIGHT_COLORS north_t;
+	LIGHT_COLORS east_s;
+	LIGHT_COLORS east_t;
+	LIGHT_COLORS south_s;
+	LIGHT_COLORS south_t;
+	LIGHT_COLORS west_s;
+	LIGHT_COLORS west_t;
+};
+
+struct TRAFFIC_SENSORS
+{
+	int ns_straight;
+	int ns_turn;
+	int ns_pedestrian;
+	int ew_straight;
+	int ew_turn;
+	int ew_pedestrian;
+	int train;
+};
+
+struct traffic_data
+{
+	TRAFFIC_STATES current_state;
+	TRAFFIC_STATES next_state;
+	TRAFFIC_STATES prev_state;
+
+	TRAFFIC_TIMING timing;
+	TRAFFIC_LIGHTS lights;
+	TRAFFIC_SENSORS sensors;
+};
 
 /*-----------------------------------------------------------------------------
 * Threads Declarations
@@ -68,8 +107,11 @@ typedef struct
 /*-----------------------------------------------------------------------------
 * Local Function Declarations
 *---------------------------------------------------------------------------*/
-void print_state(TRAFFIC_STATES *current_state);
-void trafficlights_change(TRAFFIC_STATES *current_state);
+void state_display(traffic_data *data);
+void state_perform(traffic_data *data);
+void state_transition(traffic_data *data);
+
+void init_traffic_data(traffic_data *data);
 
 /*-----------------------------------------------------------------------------
 * Main Function
@@ -80,14 +122,13 @@ int main(void)
 
 	traffic_data traffic;
 
-	traffic.current_state = INITIALIZE;
-	traffic.counter = 0;
-	traffic.sensor = '0';
+	init_traffic_data(&traffic);
 
 	for (int i = 0; i < 10; i++)
 	{
-		print_state( &traffic.current_state );
-		trafficlights_change( &traffic.current_state );
+		state_display( &traffic );
+		state_perform( &traffic );
+		state_transition( &traffic );
 	}
 
 	printf("Traffic Lights - Intersection 1 - Main terminated.\n");
@@ -102,89 +143,444 @@ int main(void)
 /*-----------------------------------------------------------------------------
 * Local Function Definitions
 *---------------------------------------------------------------------------*/
-void print_state(TRAFFIC_STATES *current_state)
+void state_display(traffic_data *data)
 {
-	switch(*current_state)
+	switch(data->current_state)
 	{
-	case INITIALIZE:
-		// EWR-NSR(0)
+	case DEFAULT:
 		// Initial State
-		printf("Current state: EWR-NSR(0)\t Lights: EW(RED) \tNS(RED)\n");
+		printf("Current state: DEFAULT\t\n");
 		break;
-	case EWNS_STOP:
-		// EWR-NSR(1)
-		printf("Current state: EWR-NSR(1)\t Lights: EW(RED) \tNS(RED)\n");
+	case NSG:
+		// North South Green
+		printf("Current state: NSG\t(North South Green)\n");
 		break;
-	case EWG_NSR:
-		// EWG-NSR(2)
-		printf("Current state: EWG-NSR(2)\t Lights: EW(GREEN) \tNS(RED)\n");
+	case NSY:
+		// North South Yellow
+		printf("Current state: NSG\t(North South Yellow)\n");
 		break;
-	case EWY_NSR:
-		// EWY-NSR(3)
-		printf("Current state: EWY-NSR(3)\t Lights: EW(YELLOW) \tNS(RED)\n");
+	case NSTG:
+		// North South Turn Green
+		printf("Current state: NSTG\t(North South Turn Green)\n");
 		break;
-	case EWR_NSR:
-		// EWR-NSR(4)
-		printf("Current state: EWR-NSR(4)\t Lights: EW(RED) \tNS(RED)\n");
+	case NSTY:
+		// North South Turn Yellow
+		printf("Current state: NSTY\t(North South Turn Yellow)\n");
 		break;
-	case EWR_NSG:
-		// EWR-NSG(5)
-		printf("Current state: EWR-NSG(5)\t Lights: EW(RED) \tNS(GREEN)\n");
+	case EWG:
+		// East West Green
+		printf("Current state: EWG\t(East West Green Green)\n");
 		break;
-	case EWR_NSY:
-		// EWR-NSY(6)
-		printf("Current state: EWR-NSY(6)\t Lights: EW(RED) \tNS(YELLOW)\n");
+	case EWY:
+		// East West Yellow
+		printf("Current state: EWY\t(East West Yellow)\n");
+		break;
+	case EWTG:
+		// East West Turn Green
+		printf("Current state: EWTG\t(East West Turn Green)\n");
+		break;
+	case EWTY:
+		// East West Turn Yellow
+		printf("Current state: EWTY\t(East West Turn Yellow)\n");
+		break;
+	case RED:
+		// All Red
+		printf("Current state: RED\t(All Red)\n");
+		break;
+	case ERROR:
+		// Error State
+		printf("Current state: ERROR\n");
 		break;
 	default:
 		// Unknown State
-		printf("ERROR: Unknown state\n");
+		printf("state_display: Unknown state\n");
 	}
 	fflush(stdout);
 }
 
-void trafficlights_change(TRAFFIC_STATES *current_state)
+void state_perform(traffic_data *data)
 {
-	switch(*current_state)
+	switch(data->current_state)
 	{
-	case INITIALIZE:
-		// EWR-NSR(0)
+	case DEFAULT:
 		// Initial State
-		sleep(SLEEP_RED);
-		*current_state = EWNS_STOP;
+		data->lights =	{
+				LED_OFF, 	// N Straight
+				LED_OFF, 	// N Turn
+				LED_OFF,	// E Straight
+				LED_OFF, 	// E Turn
+				LED_OFF,	// S Straight
+				LED_OFF, 	// S Turn
+				LED_OFF, 	// W Straight
+				LED_OFF		// W Turn
+		};
+
 		break;
-	case EWNS_STOP:
-		// EWR-NSR(1)
-		sleep(SLEEP_RED);
-		*current_state = EWG_NSR;
+	case NSG:
+		// North South Green
+		data->lights =	{
+				LED_GREEN, 	// N Straight
+				LED_RED, 	// N Turn
+				LED_RED,	// E Straight
+				LED_RED, 	// E Turn
+				LED_GREEN,	// S Straight
+				LED_RED, 	// S Turn
+				LED_RED, 	// W Straight
+				LED_RED		// W Turn
+		};
 		break;
-	case EWG_NSR:
-		// EWG-NSR(2)
-		sleep(SLEEP_GREEN);
-		*current_state = EWY_NSR;
+	case NSY:
+		// North South Yellow
+		data->lights =	{
+				LED_YELLOW, // N Straight
+				LED_RED, 	// N Turn
+				LED_RED,	// E Straight
+				LED_RED, 	// E Turn
+				LED_YELLOW,	// S Straight
+				LED_RED, 	// S Turn
+				LED_RED, 	// W Straight
+				LED_RED		// W Turn
+		};
 		break;
-	case EWY_NSR:
-		// EWY-NSR(3)
-		*current_state = EWR_NSR;
-		sleep(SLEEP_YELLOW);
+	case NSTG:
+		// North South Turn Green
+		data->lights =	{
+				LED_RED, 	// N Straight
+				LED_GREEN, 	// N Turn
+				LED_RED,	// E Straight
+				LED_RED, 	// E Turn
+				LED_RED,	// S Straight
+				LED_GREEN, 	// S Turn
+				LED_RED, 	// W Straight
+				LED_RED		// W Turn
+		};
 		break;
-	case EWR_NSR:
-		// EWR-NSR(4)
-		sleep(SLEEP_RED);
-		*current_state = EWR_NSG;
+	case NSTY:
+		// North South Turn Yellow
+		data->lights =	{
+				LED_RED, 	// N Straight
+				LED_YELLOW, // N Turn
+				LED_RED,	// E Straight
+				LED_RED, 	// E Turn
+				LED_RED,	// S Straight
+				LED_YELLOW, // S Turn
+				LED_RED, 	// W Straight
+				LED_RED		// W Turn
+		};
 		break;
-	case EWR_NSG:
-		// EWR-NSG(5)
-		sleep(SLEEP_GREEN);
-		*current_state = EWR_NSY;
+	case EWG:
+		// East West Green
+		data->lights =	{
+				LED_RED, 	// N Straight
+				LED_RED, 	// N Turn
+				LED_GREEN,	// E Straight
+				LED_RED, 	// E Turn
+				LED_RED,	// S Straight
+				LED_RED, 	// S Turn
+				LED_GREEN, 	// W Straight
+				LED_RED		// W Turn
+		};
 		break;
-	case EWR_NSY:
-		// EWR-NSY(6)
-		sleep(SLEEP_YELLOW);
-		*current_state = EWNS_STOP;
+	case EWY:
+		// East West Yellow
+		data->lights =	{
+				LED_RED, 	// N Straight
+				LED_RED, 	// N Turn
+				LED_YELLOW,	// E Straight
+				LED_RED, 	// E Turn
+				LED_RED,	// S Straight
+				LED_RED, 	// S Turn
+				LED_YELLOW, // W Straight
+				LED_RED		// W Turn
+		};
+		break;
+	case EWTG:
+		// East West Turn Green
+		data->lights =	{
+				LED_RED, 	// N Straight
+				LED_RED, 	// N Turn
+				LED_RED,	// E Straight
+				LED_GREEN, 	// E Turn
+				LED_RED,	// S Straight
+				LED_RED, 	// S Turn
+				LED_RED, 	// W Straight
+				LED_GREEN	// W Turn
+		};
+		break;
+	case EWTY:
+		// East West Turn Yellow
+		data->lights =	{
+				LED_RED, 	// N Straight
+				LED_RED, 	// N Turn
+				LED_RED,	// E Straight
+				LED_YELLOW, 	// E Turn
+				LED_RED,	// S Straight
+				LED_RED, 	// S Turn
+				LED_RED, 	// W Straight
+				LED_YELLOW	// W Turn
+		};
+		break;
+	case RED:
+		data->lights =	{
+				LED_RED, 	// N Straight
+				LED_RED, 	// N Turn
+				LED_RED,	// E Straight
+				LED_RED, 	// E Turn
+				LED_RED,	// S Straight
+				LED_RED, 	// S Turn
+				LED_RED, 	// W Straight
+				LED_RED		// W Turn
+		};
+		break;
+	case ERROR:
+		data->lights =	{
+				LED_RED, 	// N Straight
+				LED_RED, 	// N Turn
+				LED_RED,	// E Straight
+				LED_RED, 	// E Turn
+				LED_RED,	// S Straight
+				LED_RED, 	// S Turn
+				LED_RED, 	// W Straight
+				LED_RED		// W Turn
+		};
+		printf("state_perform: ERROR State");
+		break;
+	default:
+		data->lights =	{
+				LED_RED, 	// N Straight
+				LED_RED, 	// N Turn
+				LED_RED,	// E Straight
+				LED_RED, 	// E Turn
+				LED_RED,	// S Straight
+				LED_RED, 	// S Turn
+				LED_RED, 	// W Straight
+				LED_RED		// W Turn
+		};
+		printf("state_perform: Unknown State");
+	}
+	fflush(stdout);
+}
+
+void state_transition(traffic_data *data)
+{
+	// Transition States
+	data->prev_state = data->current_state;
+	data->current_state = data->next_state;
+
+	// Load Next State
+	switch(data->current_state)
+	{
+	case DEFAULT:
+		// Initial State
+		data->next_state = NSG;
+		break;
+	case NSG:
+		// North South Green
+		data->next_state = NSY;
+		break;
+	case NSY:
+		// North South Yellow
+		data->next_state = RED;
+		break;
+	case NSTG:
+		// North South Turn Green
+		data->next_state = NSTY;
+		break;
+	case NSTY:
+		// North South Turn Yellow
+		data->next_state = RED;
+		break;
+	case EWG:
+		// East West Green
+		data->next_state = EWY;
+		break;
+	case EWY:
+		// East West Yellow
+		data->next_state = RED;
+		break;
+	case EWTG:
+		// East West Turn Green
+		data->next_state = EWTY;
+		break;
+	case EWTY:
+		// East West Turn Yellow
+		data->next_state = RED;
+		break;
+	case RED:
+		// All Red State
+		if (data->prev_state == NSY)
+		{
+			if (data->sensors.train == 1)
+			{
+				// Train is present
+				data->next_state = EWG;
+			}
+			else if (data->sensors.ew_turn == 1)
+			{
+				// Car on EW wants to turn
+				data->next_state = EWTG;
+
+				// Reset Sensor
+				data->sensors.ew_turn = 0;
+			}
+			else if (data->sensors.ew_straight == 1 || data->sensors.ew_pedestrian == 1)
+			{
+				data->next_state = EWG;
+
+				// Reset Sensors
+				data->sensors.ew_straight = 0;
+				data->sensors.ew_pedestrian = 0;
+			}
+			else if (data->sensors.ns_turn == 1)
+			{
+				// NS Car wants to turn
+				data->next_state = NSTG;
+
+				// Reset Sensors
+				data->sensors.ns_turn = 0;
+			}
+		}
+		else if (data->prev_state == EWTY)
+		{
+			if (data->sensors.ew_straight == 1 || data->sensors.train)
+			{
+				// Car on EW wants to go straight, or train is approaching
+				data->next_state = EWG;
+
+				// Reset Sensor
+				data->sensors.ew_straight = 0;
+			}
+			else if (data->sensors.ns_turn == 1)
+			{
+				// NS Car wants to turn
+				data->next_state = NSTG;
+
+				// Reset sensor
+				data->sensors.ns_turn = 0;
+			}
+			else if (data->sensors.ns_straight == 1 || data->sensors.ns_pedestrian == 1)
+			{
+				// NS Car wants to go straight
+				data->next_state = NSG;
+
+				// Reset Sensor
+				data->sensors.ns_straight = 0;
+				data->sensors.ns_pedestrian = 0;
+			}
+		}
+		else if (data->prev_state == EWY)
+		{
+			if (data->sensors.train == 1)
+			{
+				// Train is present
+				data->next_state = NSTG;
+			}
+			else if (data->sensors.ns_turn == 1)
+			{
+				// EW Car wants to turn
+				data->next_state = EWTG;
+
+				// Reset sensor
+				data->sensors.ns_turn = 0;
+			}
+			else if (data->sensors.ns_pedestrian == 1 || data->sensors.ns_straight == 1)
+			{
+				// Pedestrian wants to cross or NS car wants to go straight
+				data->next_state = NSG;
+
+				// Reset Sensor
+				data->sensors.ns_pedestrian = 0;
+				data->sensors.ns_straight = 0;
+			}
+			else if (data->sensors.ew_turn == 1)
+			{
+				// EW Car wants to turn
+				data->next_state = EWTG;
+
+				// Reset Sensor
+				data->sensors.ew_turn = 0;
+			}
+		}
+		else if (data->prev_state == NSTY)
+		{
+			if (data->sensors.train == 1)
+			{
+				// Train is present
+				data->next_state = EWG;
+			}
+			else if (data->sensors.ns_straight == 1 || data->sensors.ns_pedestrian == 1)
+			{
+				// NS car wants to go straight or NS pedestrian wants to cross
+				data->next_state = NSG;
+
+				// Reset Sensor
+				data->sensors.ns_straight = 0;
+				data->sensors.ns_pedestrian = 0;
+			}
+			else if (data->sensors.ew_turn == 1)
+			{
+				// EW Car wants to turn
+				data->next_state = EWTG;
+
+				// Reset Sensor
+				data->sensors.ew_turn = 0;
+			}
+			else if (data->sensors.ew_straight == 1)
+			{
+				// EW car wants to go straight
+				data->next_state = EWG;
+
+				// Reset Sensor
+				data->sensors.ew_straight = 0;
+			}
+		}
+		break;
+	case ERROR:
+		// Error State
+		printf("state_transition: ERROR State\n");
 		break;
 	default:
 		// Unknown State
-		printf("ERROR: Invalid state\n");
+		printf("state_transition: Unknown state\n");
 	}
 	fflush(stdout);
+}
+
+void init_traffic_data(traffic_data *data)
+{
+	// Initialize States
+	data->current_state = DEFAULT;
+	data->next_state = DEFAULT;
+	data->prev_state = DEFAULT;
+
+	// Initialize LED Lights
+	data->lights =	{
+			LED_OFF, 	// N Straight
+			LED_OFF, 	// N Turn
+			LED_OFF,	// E Straight
+			LED_OFF, 	// E Turn
+			LED_OFF,	// S Straight
+			LED_OFF, 	// S Turn
+			LED_OFF, 	// W Straight
+			LED_OFF		// W Turn
+	};
+
+	// Initialize Timing
+	data->timing =	{
+			3,	// NS Straight
+			1,	// NS Turn
+			3,	// EW Straight
+			1	// EW Turn
+	};
+
+	// Initialize Sensors
+	data->sensors = {
+			0,	// NS Straight
+			0,	// NS Turn
+			0,	// NS Pedestrian
+			0,	// EW Straight
+			0,	// EW Turn
+			0,	// EW Pedestrian
+			0	// Train
+	};
 }
