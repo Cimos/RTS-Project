eb9d3e0d9ac519ca3af90834fba4f5fa46edb8a8
Shawn Buschmann <shawnbuschmann23@gmail.com>
2018-10-14 19:34:39 +1100

    basic IPC into train intersection

--- train_intersection_1/src/train_intersection_1.cpp
+++ train_intersection_1/src/train_intersection_1.cpp
@@ -11,6 +11,7 @@
 * 			Shawn Buschmann	s3478646
 */
 
+
 /*-----------------------------------------------------------------------------
 * Included Files
 *---------------------------------------------------------------------------*/
@@ -20,7 +21,8 @@
 #include <keyPad.h>
 #include <iostream>
 #include <pthread.h>
-#include <vector>
+#include <sys/iofunc.h>
+#include <errno.h>
 
 #include "threadTemplate.h"
 #include "DelayTimer.h"
@@ -30,7 +32,7 @@ using namespace std;
 /*-----------------------------------------------------------------------------
 * Definitions
 *---------------------------------------------------------------------------*/
-
+#define IPC_BUFF_SIZE 100
 
 /*-----------------------------------------------------------------------------
 * Local Variables and Buffers
@@ -41,6 +43,26 @@ enum states{
 	boomAlarmState,
 	boomgateDownState
 };
+
+typedef struct
+{
+	struct _pulse hdr; // Our real data comes after this header
+	int ClientID; // our data (unique id from client)
+	char data;     // our data
+} my_data;
+
+typedef struct
+{
+	struct _pulse hdr;  // Our real data comes after this header
+	char buf[IPC_BUFF_SIZE]; // Message we send back to clients to tell them the messages was processed correctly.
+} my_reply;
+
+//for sending PID and channel ID to client
+typedef struct
+{
+	int PID;
+	int CHID;
+} file_params;
 
 int currentState, oldCurrentState = initState;
 bool train_1_arrive_1 = false;
@@ -54,6 +76,8 @@ char KeyPress = 0;
 int keepLooping = 1;
 char lastSensorInputReset = '0';
 WorkerThread keyPadBuffer;
+my_data controlHubMsg;
+my_data *pmsg;
 
 
 
@@ -76,6 +100,7 @@ DelayTimer inboundSensorFaultTimer(true, 0, 2, 0, 2); //inbound train sensor fau
 DelayTimer twoMilSecTimer(true, 1, 2000000,1,2000000);
 void *work_cb(workBuf *work);
 void keypad_cb(char keypress);
+int server();
 
 
 /*-----------------------------------------------------------------------------
@@ -107,32 +132,13 @@ int main() {
 	void *SMThreadRetval;
 	pthread_create(&SMThread, NULL, trainStateMachine_ex, NULL);
 
-	/*
-	//setup sensor input thread
-	pthread_attr_t sensorInput_attr;
-	struct sched_param sensorInput_param;
-	pthread_attr_init(&sensorInput_attr);
-	pthread_attr_setschedpolicy(&sensorInput_attr, SCHED_RR);
-	sensorInput_param.sched_priority = 20;
-	pthread_attr_setschedparam (&sensorInput_attr, &sensorInput_param);
-	pthread_attr_setinheritsched (&sensorInput_attr, PTHREAD_EXPLICIT_SCHED);
-	pthread_attr_setstacksize (&sensorInput_attr, 8000);
-
-	pthread_t sesorInputThread;
-	void *sensorInputRetval;
-	pthread_create(&sesorInputThread, &sensorInput_attr, sensorInput_ex, NULL);
-	*/
-
-	while(1){
-
-		sleep(1);
-	}
-
+	//server code
+	int ret = 0;
+	ret = server();
 
 
 	//close threads
 	pthread_join(SMThread, &SMThreadRetval);
-	//pthread_join(sesorInputThread, &sensorInputRetval);
 
 	printf("\nMain Terminating...\n");
 	return EXIT_SUCCESS;
@@ -143,16 +149,16 @@ int main() {
 *---------------------------------------------------------------------------*/
 
 void *trainStateMachine_ex(void *data){
+	int done = 0;
 	trainStateMachine();
+	while (!done){
+		if (controlHubMsg.data == 'q'){		//terminate program condition
+			done = 1;
+		}
+	}
 	return 0;
 }
 
-/*
-void *sensorInput_ex(void *data){
-	getSensorInput();
-	return 0;
-}
-*/
 
 /*-----------------------------------------------------------------------------
 * Local Function Definitions
@@ -174,7 +180,8 @@ void *work_cb(workBuf *work)
 void keypad_cb(char keypress){
  	//cout << keypress << endl;
  	keyPadBuffer.doWork(&keypress, sizeof(keypress), 0);
- 	usleep(1);
+ 	//usleep(1);
+ 	twoMilSecTimer.createTimer();
 }
 
 /*
@@ -201,7 +208,20 @@ int printCurrentState(int currentState){
 }
 */
 
-
+/*
+ * getInputSensor() Key pad bindings:
+ * 1 = Train 1 Inbound sensor 1
+ * 2 = Train 1 Inbound sesnor 2
+ * 3 = Train 1 Boom gate down sensor
+ * 4 = Train 1 Boomgate down Sensor
+ * 5 = Train 1 Outbound sensor
+ *
+ * 9  = Train 2 Inbound sensor 1
+ * 10 = Train 2 Inbound sesnor 2
+ * 11 = Train 2 Boom gate down sensor
+ * 12 = Train 2 Boomgate down Sensor
+ * 13 = Train 2 Outbound sensor
+ */
 void getSensorInput(char input){
 	//sensorInput = getchar();			//if using keyboard
 	sensorInput = input;				//if using keyPad
@@ -256,19 +276,20 @@ void getSensorInput(char input){
 void trainStateMachine(){
 
 	char lastSensorInput = lastSensorInputReset;
+	char oldMsgData = lastSensorInputReset;
 
 	while (keepLooping)
 	{
-		//getSensorInput();					//read key pad
 
-		if(sensorInput != lastSensorInput) 	// change state if new input
+		if((sensorInput != lastSensorInput) || (controlHubMsg.data != oldMsgData)) 	// change state if new input
 		{
+			oldMsgData = controlHubMsg.data;
 			lastSensorInput = sensorInput;
 		}
 		else								// no new input - back to top
 		{
-			//twoMilSecTimer.createTimer();
-			usleep(1);
+			twoMilSecTimer.createTimer();
+			//usleep(1);
 			continue;
 		}
 
@@ -282,11 +303,11 @@ void trainStateMachine(){
 			break;
 
 		case boomgateUpState:
-			if (train_1_arrive_1 || train_1_arrive_2)
+			if ((train_1_arrive_1 || train_1_arrive_2))
 			{
 				currentState = boomAlarmState;
 				inboundSensorFaultTimer.createTimer();
-				//getSensorInput();
+
 				if(!train_1_arrive_1 || !train_1_arrive_2)
 				{
 					cout << "Train 1 In-Bound sensor fault" << endl;	//TODO send sensor fault message here
@@ -303,6 +324,12 @@ void trainStateMachine(){
 				}
 				lastSensorInput = lastSensorInputReset;
 			}
+			else if (controlHubMsg.data == 'i')		//control hub boom down request
+			{
+				cout << "control Hub message received: " << controlHubMsg.data << endl;
+				currentState = boomAlarmState;
+				controlHubMsg.data = lastSensorInputReset;
+			}
 			else{
 				currentState = boomgateUpState;
 				cout << "BOOMGATE UP" << endl;
@@ -315,13 +342,13 @@ void trainStateMachine(){
 			boomGateTimer.createTimer();
 			if((train_1_boom_down == false) || (train_2_boom_down == false))
 			{
-				cout << "Boomgate 1 DOWN FAULT" << endl;		//TODO send fault message
+				cout << "Boomgate DOWN FAULT" << endl;			//TODO send fault message
 			}													//TODO turn on in bound train stop lights
 			lastSensorInput = lastSensorInputReset;
 			break;
 
 		case boomgateDownState:
-			if ((!train_1_arrive_1 && !train_1_arrive_2) && (!train_2_arrive_1 && !train_2_arrive_2))
+			if ((!train_1_arrive_1 && !train_1_arrive_2) && (!train_2_arrive_1 && !train_2_arrive_2) && (controlHubMsg.data != '0'))
 			{
 				currentState = boomgateUpState;
 				cout << "TRAIN OUTBOUND" << endl;
@@ -347,5 +374,163 @@ void trainStateMachine(){
 	}
 }
 
+
+/*** Server code ***/
+int server()
+{
+	// Get PID and create CHID
+	int serverPID = 0, chid = 0; 	// Server PID and channel ID
+	serverPID = getpid(); 		// get server process ID
+
+	// Create Channel
+	chid = ChannelCreate(_NTO_CHF_DISCONNECT);
+	if (chid == -1)  // _NTO_CHF_DISCONNECT flag used to allow detach
+	{
+		printf("\nFailed to create communication channel on server\n");
+		return EXIT_FAILURE;
+	}
+
+	printf("Server Listening for Clients on:\n");
+	//printf("These printf statements can be removed when the myServer.info file is implemented\n");
+	printf("  --> Process ID   : %d \n", serverPID);
+	printf("  --> Channel ID   : %d \n", chid);
+
+	/*
+	*   Your code here to write this information to a file at a known location so a client can grab it...
+	*
+	*   The data should be written to a file like:
+	*   /tmp/myServer.info
+	*	 serverPID  (first line of file)
+	*	 Channel ID (second line of file)
+	*/
+	//**************************************************************************************************
+	// create file and populate with PID and CHID
+	FILE *fp;
+	file_params my_file;
+	my_file.CHID = chid;
+	my_file.PID = serverPID;
+	int file_test = 0;
+
+	//create file to write "w" to names shawns file
+	fp = fopen("/net/BBB_CimosDirect/fs/shawn_file", "w");
+
+	//write to file
+	file_test = fwrite(&my_file, sizeof(file_params), 1, fp);
+
+	fclose(fp);
+
+
+	//**************************************************************************************************
+
+
+	int rcvid = 0, msgnum = 0;  	// no message received yet
+	int Stay_alive = 0, living = 0;	// server stays running (ignores _PULSE_CODE_DISCONNECT request)
+
+	my_reply replymsg; 			// replymsg structure for sending back to client
+	replymsg.hdr.type = 0x01;
+	replymsg.hdr.subtype = 0x00;
+
+	living = 1;
+	while (living)
+	{
+		// Do your MsgReceive's here now with the chid
+		rcvid = MsgReceive(chid, &controlHubMsg, sizeof(controlHubMsg), NULL);
+
+		if (rcvid == -1)  // Error condition, exit
+		{
+			printf("\nFailed to MsgReceive\n");
+			break;
+		}
+
+		// did we receive a Pulse or message?
+		// for Pulses:
+		if (rcvid == 0)  //  Pulse received, work out what type
+		{
+			switch (controlHubMsg.hdr.code)
+			{
+			case _PULSE_CODE_DISCONNECT:
+				// A client disconnected all its connections by running
+				// name_close() for each name_open()  or terminated
+				if (Stay_alive == 0)
+				{
+					ConnectDetach(controlHubMsg.hdr.scoid);
+					printf("\nServer was told to Detach from ClientID:%d ...\n", controlHubMsg.ClientID);
+					living = 0; // kill while loop
+					continue;
+				}
+				else
+				{
+					printf("\nServer received Detach pulse from ClientID:%d but rejected it ...\n", controlHubMsg.ClientID);
+				}
+				break;
+
+			case _PULSE_CODE_UNBLOCK:
+				// REPLY blocked client wants to unblock (was hit by a signal
+				// or timed out).  It's up to you if you reply now or later.
+				printf("\nServer got _PULSE_CODE_UNBLOCK after %d, msgnum\n", msgnum);
+				break;
+
+			case _PULSE_CODE_COIDDEATH:  // from the kernel
+				printf("\nServer got _PULSE_CODE_COIDDEATH after %d, msgnum\n", msgnum);
+				break;
+
+			case _PULSE_CODE_THREADDEATH: // from the kernel
+				printf("\nServer got _PULSE_CODE_THREADDEATH after %d, msgnum\n", msgnum);
+				break;
+
+			default:
+				// Some other pulse sent by one of your processes or the kernel
+				printf("\nServer got some other pulse after %d, msgnum\n", msgnum);
+				break;
+
+			}
+			continue;// go back to top of while loop
+		}
+
+		// for messages:
+		if (rcvid > 0) // if true then A message was received
+		{
+			msgnum++;
+
+			// If the Global Name Service (gns) is running, name_open() sends a connect message. The server must EOK it.
+			if (controlHubMsg.hdr.type == _IO_CONNECT)
+			{
+				MsgReply(rcvid, EOK, NULL, 0);
+				printf("\n gns service is running....");
+				continue;	// go back to top of while loop
+			}
+
+			// Some other I/O message was received; reject it
+			if (controlHubMsg.hdr.type > _IO_BASE && controlHubMsg.hdr.type <= _IO_MAX)
+			{
+				MsgError(rcvid, ENOSYS);
+				printf("\n Server received and IO message and rejected it....");
+				continue;	// go back to top of while loop
+			}
+
+			// A message (presumably ours) received
+
+			// put your message handling code here and assemble a reply message
+			//sprintf(replymsg.buf, "Message %d received", msgnum);
+			//printf("Server received data packet with value of %c from client (ID:%d), ", msg.data, msg.ClientID);
+			//fflush(stdout);
+			//sleep(1); // Delay the reply by a second (just for demonstration purposes)
+
+			//printf("\n    -----> replying with: '%s'\n",replymsg.buf);
+			MsgReply(rcvid, EOK, &replymsg, sizeof(replymsg));
+		}
+		else
+		{
+			printf("\nERROR: Server received something, but could not handle it correctly\n");
+		}
+
+	}
+
+	printf("\nServer received Destroy command\n");
+	// destroyed channel before exiting
+	ChannelDestroy(chid);
+
+	return EXIT_SUCCESS;
+}
 
 
---
+++ train_test_IPC_client/src/train_test_IPC_client.cpp
@@ -0,0 +1,171 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <sys/iofunc.h>
+#include <sys/netmgr.h>
+
+#define BUF_SIZE 100
+
+typedef struct
+{
+	struct _pulse hdr; // Our real data comes after this header
+	int ClientID; // our data (unique id from client)
+	char data[2];     // our data
+} my_data;
+
+typedef struct
+{
+	struct _pulse hdr; // Our real data comes after this header
+	char buf[BUF_SIZE];// Message we send back to clients to tell them the messages was processed correctly.
+} my_reply;
+
+typedef struct
+{
+	int PID;
+	int CHID;
+} file_params;
+
+size_t read_data(FILE *fp, file_params *p)
+{
+	return(fread(p, sizeof(file_params), 1, fp));
+}
+
+// prototypes
+int client(int serverPID, int serverCHID);
+
+int main(int argc, char *argv[])
+{
+	printf("Client running\n");
+
+	// connection data (you may need to edit this)
+	int serverPID = 0;	// CHANGE THIS Value to PID of the server process
+	int	serverCHID = 0;			// CHANGE THIS Value to Channel ID of the server process (typically 1)
+
+	/*
+	*   Your code goes here.
+	*
+	*   You need to find the process ID (PID), and a channel ID (CHID) of the server to make a connection to it...
+	*
+	*   The data should be read from file on the server (for example it could be located at):
+	*
+	*	 serverPID  (first line of file)
+	*	 Channel ID (second line of file)
+	*
+	*   Initially you may NEED TO START THE SERVER and set the serverPID = Process ID of the RUNNING SERVER
+	*
+	*/
+	//********************************************************************************************
+	FILE *fp;
+	file_params my_file;
+
+	//open file
+	fp = fopen("/net/BBB_CimosDirect/fs/shawn_file", "r");
+
+	//read in PID and CHID from file
+	if (fp != NULL){
+		while (read_data(fp, &my_file) != 0){
+			printf("\nPID = %d", my_file.PID);
+			printf("\nCHAN ID = %d\n\n", my_file.CHID);
+			serverPID = my_file.PID;
+			serverCHID = my_file.CHID;
+		}
+
+		fclose(fp);
+	}
+
+	//********************************************************************************************
+
+
+	int ret = 0;
+	ret = client(serverPID, serverCHID);
+
+	printf("Main (client) Terminated....\n");
+	return ret;
+}
+
+/*** Client code ***/
+int client(int serverPID, int serverChID)
+{
+	my_data msg;
+	my_reply reply;
+
+	msg.ClientID = 999;
+
+	int server_coid;
+	int index = 0;
+
+	printf("   --> Trying to connect (server) process which has a PID: %d\n", serverPID);
+	printf("   --> on channel: %d\n\n", serverChID);
+
+	// set up message passing channel
+	server_coid = ConnectAttach(ND_LOCAL_NODE, serverPID, serverChID, _NTO_SIDE_CHANNEL, 0);
+	if (server_coid == -1)
+	{
+		printf("\n    ERROR, could not connect to server!\n\n");
+		return EXIT_FAILURE;
+	}
+
+
+	printf("Connection established to process with PID:%d, Ch:%d\n", serverPID, serverChID);
+
+	// We would have pre-defined data to stuff here
+	msg.hdr.type = 0x00;
+	msg.hdr.subtype = 0x00;
+
+	int done = 0;
+	char keyboardInput = '0';
+
+	// Do whatever work you wanted with server connection
+	//for (index=0; index < 5; index++) // send data packets
+	//{
+	// set up data packet
+	//msg.data=10+index;
+
+	while (!done){
+		while (1){
+			scanf("%c", &keyboardInput);					//wait for keyboard input
+			if (keyboardInput == 'i'){
+				msg.data[0] = keyboardInput;
+				//sprintf(buf, "%c", keyboardInput);			//put the message in a char[] so it can be sent
+				break;
+			}
+			else if (keyboardInput == 'o'){
+				msg.data[0] = keyboardInput;
+				//sprintf(buf, "%c", keyboardInput);			//put the message in a char[] so it can be sent
+				break;
+			}
+			else if (keyboardInput == 'q'){					// press q to close queue and quit
+				//sprintf(buf, "%c", keyboardInput);
+				msg.data[0] = keyboardInput;
+				done = 1;
+				break;
+			}
+		}
+		keyboardInput = '0';
+
+
+		// the data we are sending is in msg.data
+		printf("Client (ID:%d), sending data packet with the  value: %c \n", msg.ClientID, msg.data[0]);
+		fflush(stdout);
+
+		if (MsgSend(server_coid, &msg, sizeof(msg), &reply, sizeof(reply)) == -1)
+		{
+			printf(" Error data '%d' NOT sent to server\n", msg.data[0]);
+			// maybe we did not get a reply from the server
+			break;
+		}
+		else
+		{ // now process the reply
+			//printf("   -->Reply is: '%s'\n", reply.buf);
+		}
+
+	}//end while
+	//sleep(5);	// wait a few seconds before sending the next data packet
+	//}
+
+	// Close the connection
+	printf("\n Sending message to server to tell it to close the connection\n");
+	ConnectDetach(server_coid);
+
+	return EXIT_SUCCESS;
+}
